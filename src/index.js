d138058c74f04142ca3c685cf9d17d113fe5fecd
Jeffimenson <jeffengim@gmail.com>
2023-12-29 11:35:42 -0500

    Make functional querying function in js code to weatherapi

--- src/index.js
+++ src/index.js
@@ -1,3 +1,57 @@
 import './style.css';
+import { format, parseISO } from 'date-fns';
 
-console.log('Hola el mundo.');
+// Weatherapi key
+const API_KEY = 'acb745e75bc74a35bd612350232312';
+
+async function loadData(city) {
+    try {
+        const response = await fetch(
+            `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}&days=3&aqi=no&alerts=no`,
+            { mode: 'cors' }
+        );
+        const data = await response.json();
+        const days = data.forecast.forecastday;
+
+        const dailyData = [];
+        for (let i = 0; i < days.length; i += 1) {
+            const dayData = days[i];
+            const { date } = dayData;
+            const condition = dayData.day.condition.text;
+            const avgTempF = dayData.day.avgtemp_f;
+            const rainChance = dayData.day.daily_chance_of_rain;
+            const snowChance = dayData.day.daily_chance_of_snow;
+
+            const hourlyData = [];
+            for (let j = 0; j < dayData.hour.length; j += 1) {
+                const hourDate = dayData.hour[j].time;
+                const parsedDate = parseISO(hourDate);
+
+                const hourData = {
+                    time: format(parsedDate, 'h:mm a'),
+                    tempF: dayData.hour[j].temp_f,
+                    condition: dayData.hour[j].condition.text,
+                };
+                hourlyData.push(hourData);
+            }
+
+            dailyData.push({
+                date,
+                avgTempF,
+                condition,
+                rainChance,
+                snowChance,
+                hourlyData,
+            });
+        }
+
+        return dailyData;
+    } catch (err) {
+        console.error(err);
+        return err;
+    }
+}
+
+loadData('North Charleston').then((data) => {
+    console.log(data);
+});
